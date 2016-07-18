8b8fbf051568bfee295c0b3d180a9aef90fab1d2
Helen Che <helenc@berkeley.edu>
2016-07-18 13:03:34 -0700

    Create tag table and model and join models/tables for users and projects

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,11 @@
 class Tag < ActiveRecord::Base
+  has_many :user_tags
+  has_many :users, through: :user_tags
+
+  has_many :project_tags
+  has_many :projects, through: :project_tags
+
+  enum programming_language: [:Java, :Python, :C, :Ruby, :Javascript]
+  enum courses: [:CS61A, :Data8]
+  enum domain: []
 end
--- db/migrate/20160713000053_create_tags.rb
+++ db/migrate/20160713000053_create_tags.rb
@@ -1,7 +1,10 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
-
+      t.integer :programming_language
+      t.string :position
+      t.integer :domain
+      t.integer :courses
       t.timestamps null: false
     end
   end
