dbd1de6ceea5915791c123e4ad5c13382c7c2a3b
jc003 <jerryc@berkeley.edu>
2016-08-13 20:01:15 -0700

    more responsive plus pagination

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all
+    @projects = Project.paginate(:page => params[:page], :per_page => 1)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Project.create(title: 'Datahive - The Data Science Project Hub', subtitle: 'BIDS and DSSB Collaboration Project', startdate: '08/24/16', enddate: '08/25/16', app_deadline: '08/23/16', description: 'This is a test description', hours_per_week: 1, compensation: 0, education_level: 0, state: 1, field: 1, positions: ['Front End Developer','Web Designer','Researcher','Data Scientist','Engineer'], questions: ['Why do you want to help us?'], created_at: '08/22/16', updated_at: '08/22/16')
